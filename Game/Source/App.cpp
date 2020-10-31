bc1bacbe74c8003ea56ad9c4f67255fba39cc2f5
Kerali <vjaragrevol@gmail.com>
2020-10-31 16:49:37 +0100

    Collisions 1.0

--- Game/Source/App.cpp
+++ Game/Source/App.cpp
@@ -6,6 +6,7 @@
 #include "Audio.h"
 #include "Scene.h"
 #include "Map.h"
+#include "ModuleCollisions.h"
 
 
 #include "Defs.h"
@@ -26,6 +27,7 @@ App::App(int argc, char* args[]) : argc(argc), args(args)
 	audio = new Audio();
 	scene = new Scene();
 	map = new Map();
+	/*collisions = new ModuleCollisions();*/
 	
 	// Ordered for awake / Start / Update
 	// Reverse order of CleanUp
@@ -35,6 +37,7 @@ App::App(int argc, char* args[]) : argc(argc), args(args)
 	AddModule(audio);
 	AddModule(scene);
 	AddModule(map);
+	/*AddModule(collisions);*/
 	
 
 	// Render last to swap buffer
